337a692922a2a77dfd731e53cf94f93e89ef3c42
unknown <mhas.software@gmail.com>
2024-08-04 15:01:25 +0330

    persian messages

--- requests/models.py
+++ requests/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.core.exceptions import ValidationError
 from accounts.models import ClientProfile, NurseProfile
 
 
 class Request(models.Model):
     STATUS_CHOICES = [
-        ('PENDING', 'Pending'),
-        ('ACCEPTED', 'Accepted'),
-        ('REJECTED', 'Rejected'),
-        ('COMPLETED', 'Completed'),
-        ('CANCELLED', 'Cancelled'),
-        ('NURSING', 'Nursing'),
+        ('PENDING', 'در انتظار'),
+        ('ACCEPTED', 'پذیرفته شده'),
+        ('REJECTED', 'رد شده'),
+        ('COMPLETED', 'تکمیل شده'),
+        ('CANCELLED', 'لغو شده'),
+        ('NURSING', 'پرستاری در حال انجام'),
     ]
 
     CATEGORY_CHOICES = [
-        ('CHILD', 'Child'),
-        ('ELDERLY', 'Elderly'),
+        ('CHILD', 'فرزند خردسال'),
+        ('ELDERLY', 'والدین کهنسال'),
     ]
 
     client = models.ForeignKey(
@@ -30,16 +29,13 @@ class Request(models.Model):
     duration_hours = models.FloatField(
         validators=[MinValueValidator(0.0)],
         default=1.0,
-        help_text="Duration in hours for which the nurse is needed"
+        help_text="مدت زمان درخواست به ساعت"
     )
     request_end = models.DateTimeField()
 
     address = models.TextField()
 
     for_others = models.BooleanField(default=False)
-    other_person_name = models.CharField(max_length=255, blank=True, null=True)
-    other_person_phone_number = models.CharField(
-        max_length=11, blank=True, null=True)
 
     status = models.CharField(
         max_length=20, choices=STATUS_CHOICES, default='PENDING')
@@ -50,16 +46,12 @@ class Request(models.Model):
         max_length=10, choices=CATEGORY_CHOICES, default='CHILD')
 
     def __str__(self):
-        return f"Request {self.id} by {self.client.user.phone_number}"
+        return f"درخواست {self.id} توسط {self.client.user.phone_number}"
 
     def clean(self):
         super().clean()
-        if self.for_others:
-            if not self.other_person_name or not self.other_person_phone_number:
-                raise ValidationError(
-                    "Both other person name and phone number must be provided if 'for others' is checked.")
         if self.duration_hours <= 0:
-            raise ValidationError("Duration must be greater than 0 hours.")
+            raise ValidationError("مدت زمان باید بیشتر از 0 ساعت باشد.")
 
     class Meta:
         ordering = ['-created_date']
